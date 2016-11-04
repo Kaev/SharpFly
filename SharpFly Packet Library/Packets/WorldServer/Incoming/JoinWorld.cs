39332670a0b241e7f59f28b9dcfec32c9154f27c
Daniel Moser <daniel.moser.1603@gmail.com>
2016-11-04 22:50:58 +0100

    Added the JoinWorld packet and started with the Messenger packets

---
+++ SharpFly Packet Library/Packets/WorldServer/Incoming/JoinWorld.cs
@@ -0,0 +1,45 @@
+﻿namespace SharpFly_Packet_Library.Packets.WorldServer.Incoming
+{
+    public class JoinWorld
+    {
+        public uint WorldID;
+        public uint PlayerID;
+        public uint AuthKey;
+        public uint PartyID;
+        public uint GuildID;
+        public uint GuildWarID;
+        public uint IdOfMulti;      //ID of the channel (When using multiple chanels)?
+        public byte CharacterSlot;
+        public string CharacterName;
+        public string AccountName;
+        public string Password;
+        public uint MessengerState;
+        public int FriendsSize;
+        public uint FriendID;
+        //TODO: Friend f;
+
+        public JoinWorld(IncomingPacket packet)
+        {
+            WorldID = packet.ReadUInt();
+            PlayerID = packet.ReadUInt();
+            AuthKey = packet.ReadUInt();
+            PartyID = packet.ReadUInt();
+            GuildID = packet.ReadUInt();
+            GuildWarID = packet.ReadUInt();
+            IdOfMulti = packet.ReadUInt();
+            CharacterSlot = packet.ReadByte();
+            CharacterName = packet.ReadString();
+            AccountName = packet.ReadString();
+            Password = packet.ReadString();
+            MessengerState = packet.ReadUInt();
+            FriendsSize = packet.ReadInt();
+            FriendID = packet.ReadUInt();
+
+            for (int i = 0; i < FriendsSize; i++)
+            {
+                //TODO: packet.Read( &f, sizeof(Friend) );
+                //SetFriend( FriendID, &f)
+            }
+        }
+    }
+}
---
+++ SharpFly Packet Library/Packets/WorldServer/Incoming/MessengerAddFriend.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpFly_Packet_Library.Packets.WorldServer.Incoming
+{
+    public class MessengerAddFriend
+    {
+        public uint PlayerID;
+        public uint TargetPlayerID;
+        public byte PlayerSex;
+        public byte TargetPlayerSex;
+        public int PlayerJob;
+        public int TargetPlayerJob;
+
+        public MessengerAddFriend(IncomingPacket packet)
+        {
+            PlayerID = packet.ReadUInt();
+            TargetPlayerID = packet.ReadUInt();
+            PlayerSex = packet.ReadByte();
+            TargetPlayerSex = packet.ReadByte();
+            PlayerJob = packet.ReadInt();
+            TargetPlayerJob = packet.ReadInt();
+        }
+    }
+}
